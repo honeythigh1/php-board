c19c04dc237fd5737f62f234d1ddc45d5e963bf7
my <sdf202@naver.com>
2019-04-22 19:37:26 +0900

    first

---
+++ project/js/read.js
@@ -0,0 +1,6 @@
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script
+	src="https://code.jquery.com/jquery-3.4.0.js"
+	integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+	crossorigin="anonymous"></script>
+	
