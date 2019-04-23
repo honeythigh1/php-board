dbd63cc57ac07ef3f6a1c947a0b23193add4ae7e
my <sdf202@naver.com>
2019-04-23 18:26:41 +0900

    css, js upload

--- project/index.php
+++ project/index.php
@@ -2,7 +2,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Title of the document</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!-- 부가적인 테마 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script
+	src="https://code.jquery.com/jquery-3.4.0.js"
+	integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+	crossorigin="anonymous"></script>
 </head>
 <body>
 <?php
