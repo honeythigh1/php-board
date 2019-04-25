0612d4cbf78866b91c5d5427ce2600dedbe70e2b
2PAC <2PAC@DESKTOP-OS6ODB8>
2019-04-25 18:42:09 +0900

    .

--- project/signUp.php
+++ project/signUp.php
@@ -9,7 +9,6 @@
 <link rel="stylesheet" href="./css/css_Reset.css";>
 <link rel="stylesheet" href="./css/login.css";>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css"/>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
 </head>
@@ -32,8 +31,8 @@
 		</div>
 		
 			<div class="button">
-			   <button type="submit" class="btn btn-default"><a href="index.php">로그인</button>
-			   <button type="button" class="btn btn-default"><a href="index.php">회원가입</a></button>
+			   <button type="submit" class="btn btn-warning"><a href="index.php">로그인</button>
+			   <button type="button" class="btn btn-warning"><a href="index.php">회원가입</a></button>
 			</div>
 		</form>
 	</div>
